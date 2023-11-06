4ca1c6b6b5e9d25b247f0462394f52a23bc20cfa
KevinSilvares <k.silvares24@gmail.com>
2023-11-06 09:55:06 +0100

    App java muestra el nombre y el instituto y README muestra la modificacion

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Kevin");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("IES San Andrés");
+    System.out.println("San Andres del Rabanedo");
   }
   
 }
